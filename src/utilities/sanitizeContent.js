17f088261556ff9677e6a0e8c28b8d5c20b8e39f
Jen <jengkarlong@gmail.com>
2020-12-02 19:28:25 +0800

    replace chunk variable

--- src/utilities/sanitizeContent.js
+++ src/utilities/sanitizeContent.js
@@ -1,20 +1,19 @@
-export const sanitizeContent = (data, variables, fallbackChunk) => {
-  const parsedChunk = { ...data, content: data.content };
+export const sanitizeContent = (chunk, variables, fallbackChunk) => {
   const tokens = (chunk.content.match(/\{{(.*?)\}}/g)|| []).map(t => t.substr(2, t.length-4))
   
   let isVariableSafe = true;
   if (tokens) {
     tokens.forEach(function(token) {
       const value = variables && variables[token] || ""
-      parsedChunk.content = parsedChunk.content.replace(`{{${token}}}`, value);
+      chunk.content = chunk.content.replace(`{{${token}}}`, value);
     });
 
     if (!value) isVariableSafe = false;
   }
 
   if ( !isVariableSafe && fallbackChunk) {
-    parsedChunk.content = fallbackChunk.content
+    chunk.content = fallbackChunk.content
   }
 
-  return parsedChunk
+  return chunk
 }
