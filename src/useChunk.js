d02c8d47af8f9f6defd31c42daa98f38870d0c7a
Jen <jengkarlong@gmail.com>
2020-12-02 17:37:12 +0800

    remove submodule

--- src/useChunk.js
+++ src/useChunk.js
@@ -3,7 +3,7 @@ import { useContext, useEffect, useState, useMemo } from "react";
 
 import { EditmodeContext } from "./EditmodeContext";
 import { api, computeContentKey } from './utils'
-import { renderChunk, getCachedData, storeCache, sanitizeContent } from './utils/native'
+import { renderChunk, getCachedData, storeCache, sanitizeContent } from './utils'
 
 export function useChunk(defaultContent = "", { identifier, type, contentKey, variables }) {
   const { projectId, defaultChunks } = useContext(EditmodeContext);
